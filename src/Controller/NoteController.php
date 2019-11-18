1f5aee120c027867bcb36cc788dfd43900882007
nicordev <nicolas.renvoise.dev@gmail.com>
2019-11-18 22:52:16 +0100

    Add a delete route

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Note;
 use App\Repository\NoteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,5 +32,45 @@ class NoteController extends AbstractController
     public function show(Note $note)
     {
         return $this->render("note/show.html.twig", ["note" => $note]);
+    }
+
+    /**
+     * @Route(
+     *     "/notes/create",
+     *     name="notes_create"
+     * )
+     */
+    public function create(EntityManagerInterface $manager)
+    {
+        // TODO
+    }
+
+    /**
+     * @Route(
+     *     "/notes/edit/{id}",
+     *     name="notes_edit",
+     *     requirements={"id": "\d+"}
+     * )
+     */
+    public function edit(Note $note, EntityManagerInterface $manager)
+    {
+        // TODO
+    }
+
+    /**
+     * @Route(
+     *     "/notes/delete/{id}",
+     *     name="notes_delete",
+     *     requirements={"id": "\d+"}
+     * )
+     */
+    public function delete(Note $note, EntityManagerInterface $manager)
+    {
+        $manager->remove($note);
+        $manager->flush();
+
+        $this->addFlash("notice", "A note has been deleted.");
+
+        return $this->redirectToRoute("notes");
     }
 }
