b28db5df9a2cd6f9a4cfc5984e5421ccc6b85fcb
nicordev <nicolas.renvoise.dev@gmail.com>
2020-02-01 21:15:19 +0100

    improve: use of PasswordType::class for registration

--- src/Form/MemberRegistrationType.php
+++ src/Form/MemberRegistrationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Member;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class MemberRegistrationType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password')
+            ->add('password', PasswordType::class)
             ->add('name')
         ;
     }
