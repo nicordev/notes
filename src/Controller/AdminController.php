6170e51249987809f0d2c9578902952477a79c9f
nicordev <nicolas.renvoise.dev@gmail.com>
2020-02-04 21:46:07 +0100

    feat: add admin controller and view

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MemberRepository;
+use App\Repository\NoteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/index/{panel}", name="admin_index")
+     */
+    public function index(
+        string $panel,
+        NoteRepository $noteRepository,
+        MemberRepository $memberRepository
+    ) {
+        $notes = null;
+        $members = null;
+
+        if ($panel === "note") {
+            $notes = $noteRepository->findAll();
+        } elseif ($panel === "member") {
+            $members = $memberRepository->findAll();
+        }
+
+        return $this->render("admin/index.html.twig", [
+            "notes" => $notes,
+            "members" => $members
+        ]);
+    }
+}
