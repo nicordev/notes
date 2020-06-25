8e20f66b0f904a85c1c264a18a357ccfb7289010
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-06-25 18:03:53 +0200

    feat: add a form to add anonymous notes from home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
+use App\Form\NoteType;
+use App\Repository\NoteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,8 +15,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        NoteRepository $noteRepository,
+        Request $request,
+        EntityManagerInterface $manager
+    ) {
+        $note = new Note();
+        $noteForm = $this->createForm(
+                NoteType::class, 
+            $note
+        );
+        $noteForm->handleRequest($request);
+        $notes = $noteRepository->findBy(['author' => null], ['id' => 'DESC']);
+
+        if ($noteForm->isSubmitted() && $noteForm->isValid()) {
+            $manager->persist($note);
+            $manager->flush();
+            $this->addFlash("success", "A note has been created.");
+        }
+
+        return $this->render('home/index.html.twig', [
+            'noteForm' => $noteForm->createView(),
+            'notes' => $notes
+        ]);
     }
 }
