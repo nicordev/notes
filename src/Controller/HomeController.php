b5cc0f2ee8425ab3e1bed4abc6af92b0fafec196
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-08-14 21:35:18 +0200

    feat: add simple mod

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use App\Form\NoteType;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,21 +21,13 @@ class HomeController extends AbstractController
         Request $request,
         EntityManagerInterface $manager
     ) {
-        $note = new Note();
-        $noteForm = $this->createForm(
-                NoteType::class, 
-            $note
-        );
-        $noteForm->handleRequest($request);
+        $noteForm = $this->handleNewNoteForm($request, $manager);
 
-        if ($noteForm->isSubmitted() && $noteForm->isValid()) {
-            $manager->persist($note);
-            $manager->flush();
-            $this->addFlash("success", "A note has been created.");
+        if ($this->getParameter('app.simple_mod')) {
+            return $this->showAll($noteRepository, $noteForm);
         }
 
         $anonymousNotes = $noteRepository->findBy(['author' => null], ['id' => 'DESC']);
-
         $user = $this->getUser();
 
         if ($this->isGranted('ROLE_USER', $user)) {
@@ -46,5 +39,34 @@ class HomeController extends AbstractController
             'anonymousNotes' => $anonymousNotes,
             'userNotes' => $userNotes ?? null
         ]);
+    }
+
+    public function showAll(NoteRepository $noteRepository, FormInterface $noteForm)
+    {
+        $notes = $noteRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'noteForm' => $noteForm->createView(),
+            'anonymousNotes' => $notes,
+            'userNotes' => null
+        ]);
+    }
+
+    private function handleNewNoteForm(Request $request, EntityManagerInterface $manager)
+    {
+        $note = new Note();
+        $noteForm = $this->createForm(
+            NoteType::class,
+            $note
+        );
+        $noteForm->handleRequest($request);
+
+        if ($noteForm->isSubmitted() && $noteForm->isValid()) {
+            $manager->persist($note);
+            $manager->flush();
+            $this->addFlash("success", "A note has been created.");
+        }
+
+        return $noteForm;
     }
 }
